4031c98dba6ad6148c6cc776dc690f05696090ba
nox <ts.sarkowicz@gmail.com>
2024-10-01 11:33:50 +0000

    Different input prop

--- mouse_rs.sh
+++ mouse_rs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xinput --set-prop 9 155 1.5 0 0 0 1.5 0 0 0 1 &
-xinput --set-prop 9 'libinput Accel Profile Enabled' 0, 1 &
+xinput --set-prop 11 'libinput Accel Profile Enabled' 0, 1 &
+xinput --set-prop 11 'libinput Tapping Enabled' 1 &
