c8301a252713aea677eb4f2d053a78f104a55317
nox <ts.sarkowicz@gmail.com>
2024-10-06 23:25:23 +0200

    Bluetooth and stuff

--- checkupdates_startup.sh
+++ checkupdates_startup.sh
@@ -4,7 +4,6 @@ if [ -f ~/.config/last_checkupdates ]; then
 else
 	updates_check=""
 fi
-
 if [ "$updates_check" != "$(date +%Y-%m-%d)" ]; then
 	updates=$(yay -Qu)
 	echo $(date +%Y-%m-%d) > ~/.config/last_checkupdates
@@ -12,18 +11,18 @@ if [ "$updates_check" != "$(date +%Y-%m-%d)" ]; then
 	if [ $updates_count -gt 0 ]; then
 		if [ $updates_count -eq 1 ]; then
 			echo "There is 1 update available."
-			read -p "Would you like to view it? [T/n] " -r
+			read -p "Would you like to view it? [Y/n] " -r
 		else
 			echo "There are $updates_count updates available."
-			read -p "Would you like to view them? [T/n] " -r
+			read -p "Would you like to view them? [Y/n] " -r
 		fi
 		echo
-		if [[ $REPLY =~ ^[Tt]$ ]]; then
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
 			echo "$updates"
 		fi
 		read -p "Would you like to update? [T/n] " -r
 		echo
-		if [[ $REPLY =~ ^[Tt]$ ]]; then
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
 			sudo pacman -Syu
 			yay -Syu
 			~/Scripts/reset_settings.sh
--- mouse_rs.sh
+++ mouse_rs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xinput --set-prop 11 'libinput Accel Profile Enabled' 0, 1 &
-xinput --set-prop 11 'libinput Tapping Enabled' 1 &
+xinput --set-prop 10 'libinput Accel Profile Enabled' 0, 1 &
+xinput --set-prop 10 'libinput Tapping Enabled' 1 &
