2bc5c78ea60e62874c6feb609339d0bc71666a20
nox <ts.sarkowicz@gmail.com>
2024-10-08 11:12:28 +0200

    Change confirmation to eng

--- checkupdates.sh
+++ checkupdates.sh
@@ -6,18 +6,18 @@ updates_count=$(echo "$updates" | grep -c .)
 if [ $updates_count -gt 0 ]; then
 	if [ $updates_count -eq 1 ]; then
 		echo "There is 1 update available."
-		read -p "Would you like to view it? [T/n] " -r
+		read -p "Would you like to view it? [Y/n] " -r
 	else
 		echo "There are $updates_count updates available."
-		read -p "Would you like to view them? [T/n] " -r
+		read -p "Would you like to view them? [Y/n] " -r
 	fi
 	echo
-	if [[ $REPLY =~ ^[Tt]$ ]]; then
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		echo "$updates"
 	fi
-	read -p "Would you like to update? [T/n] " -r
+	read -p "Would you like to update? [Y/n] " -r
 	echo
-	if [[ $REPLY =~ ^[Tt]$ ]]; then
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		sudo pacman -Syu
 		yay -Syu
 		/home/nox/Scripts/reset_settings.sh
