ca5099101c8af7edaf85cc29afe445599f38ff39
Tomasz Sarkowicz <nox@noxsoftware.pl>
2024-12-26 14:45:46 +0100

    check updates improvements

--- checkupdates.sh
+++ checkupdates.sh
@@ -1,25 +1,48 @@
 #!/bin/bash
-yay -Sy
-updates=$(yay -Qu)
-echo $(date +%Y-%m-%d) > ~/.config/last_checkupdates
-updates_count=$(echo "$updates" | grep -c .)
-if [ $updates_count -gt 0 ]; then
-	if [ $updates_count -eq 1 ]; then
-		echo "There is 1 update available."
+updates_official=$(checkupdates)
+updates_aur=$(yay -Qum)
+updates_count_official=$(echo "$updates_official" | grep -c .)
+updates_count_aur=$(echo "$updates_aur" | grep -c .)
+if [ $updates_count_official -gt 0 || $updates_count_aur -gt 0 ]; then
+	if [ $updates_count_official -eq 1 && $updates_count_aur -lt 1 ]; then
+		echo "There is 1 official update available."
 		read -p "Would you like to view it? [T/n] " -r
-	else
-		echo "There are $updates_count updates available."
+	fi
+	if [  $updates_count_official -gt 1 && $updates_count_aur -lt 1 ]; then
+		echo "There are $updates_count_official updates available."
+		read -p "Would you like to view them? [T/n] " -r
+	fi
+	if [ $updates_count_official -eq 1 && $updates_count_aur -eq 1 ]; then
+		echo "There is 1 official update and 1 AUR update available."
+		read -p "Would you like to view them? [T/n] " -r
+	fi
+	if [ $updates_count_official -gt 1 && $updates_count_aur -eq 1 ]; then
+		echo "There are $updates_count_official official updates and 1 AUR update available."
+		read -p "Would you like to view them? [T/n] " -r
+	fi
+	if [ $updates_count_official -eq 1 && $updates_count_aur -gt 1 ]; then
+		echo "There is 1 official update and $updates_count_aur AUR updates available."
+		read -p "Would you like to view them? [T/n] " -r
+	fi
+	if [ $updates_count_official -gt 1 && $updates_count_aur -gt 1 ]; then
+		echo "There are $updates_count_official official updates and $updates_count_aur AUR updates available."
 		read -p "Would you like to view them? [T/n] " -r
 	fi
 	echo
 	if [[ $REPLY =~ ^[Tt]$ ]]; then
-		echo "$updates"
+		if [ $updates_count_official -gt 0 ]; then
+			echo "Official updates:"
+			echo "$updates_official"
+		fi
+		if [ $updates_count_aur -gt 0 ]; then
+			echo "AUR updates:"
+			echo "$updates_aur"
+		fi
 	fi
 	read -p "Would you like to update? [T/n] " -r
 	echo
 	if [[ $REPLY =~ ^[Tt]$ ]]; then
-		sudo pacman -Syu
 		yay -Syu
-		/home/nox/Scripts/reset_settings.sh
+		~/Scripts/reset_settings.sh
 	fi
 fi
--- checkupdates_startup.sh
+++ checkupdates_startup.sh
@@ -6,29 +6,7 @@ else
 fi
 
 if [ "$updates_check" != "$(date +%Y-%m-%d)" ]; then
-	yay -Sy
-	updates=$(yay -Qu)
 	echo $(date +%Y-%m-%d) > /home/nox/.config/last_checkupdates
-	updates_count=$(echo "$updates" | grep -c .)
-	if [ $updates_count -gt 0 ]; then
-		if [ $updates_count -eq 1 ]; then
-			echo "There is 1 update available."
-			read -p "Would you like to view it? [T/n] " -r
-		else
-			echo "There are $updates_count updates available."
-			read -p "Would you like to view them? [T/n] " -r
-		fi
-		echo
-		if [[ $REPLY =~ ^[Tt]$ ]]; then
-			echo "$updates"
-		fi
-		read -p "Would you like to update? [T/n] " -r
-		echo
-		if [[ $REPLY =~ ^[Tt]$ ]]; then
-			sudo pacman -Syu
-			yay -Syu
-			/home/nox/Scripts/reset_settings.sh
-		fi
-	fi
+	~/Scripts/checkupdates.sh
 fi
 
