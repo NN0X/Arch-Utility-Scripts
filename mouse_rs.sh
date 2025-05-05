bde218d82a4d94949efe15b329c1f580ab1211d0
nox <ts.sarkowicz@gmail.com>
2025-05-05 13:46:19 +0200

    update

--- mouse_rs.sh
+++ mouse_rs.sh
@@ -3,3 +3,5 @@ xinput --set-prop 10 'libinput Accel Profile Enabled' 0, 1 &
 xinput --set-prop 10 'libinput Tapping Enabled' 1 &
 xinput --set-prop 11 'libinput Accel Profile Enabled' 0, 1 &
 xinput --set-prop 11 'libinput Tapping Enabled' 1 &
+xinput --set-prop 12 'libinput Accel Profile Enabled' 0, 1 &
+xinput --set-prop 12 'libinput Tapping Enabled' 1 &
---
+++ navicat.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+run-desktop /home/nox/.cache/yay/navicat16-premium-en/navicat16.desktop
