ec7003051d77dc55430d16844156a943b774f94c
Tomasz Sarkowicz <nox@noxsoftware.pl>
2026-06-21 14:54:18 +0200

    sync

--- navicat.sh
+++ navicat.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+<<<<<<< Updated upstream
 run-desktop ~/.cache/yay/navicat16-premium-en/navicat16.desktop
+=======
+run-desktop /home/nox/.cache/yay/navicat16-premium-en/navicat16.desktop
+>>>>>>> Stashed changes
